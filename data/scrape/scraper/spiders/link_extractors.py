fffb63c27665ac9fec6f0e6e4da83fd487c48d22
James Harwood <jamesrharwood@gmail.com>
2021-03-27 10:10:35 +0000

    fixed allowed domains

--- data/scrape/scraper/spiders/link_extractors.py
+++ data/scrape/scraper/spiders/link_extractors.py
@@ -47,18 +47,14 @@ NOT_ALLOWED_LINKS = join_link_regexps(
 ALLOWED_EXTENSIONS = [".pdf", ".doc", ".docx", ""]
 
 ALLOWED_TEXT = join_with_or(ALLOWED_LINKS)
-ALLOWED_DOMAINS = ["elsevier.com"]
-
-
-def get_domain_from_url(url):
-    return urlparse(url).netloc
+ALLOWED_DOMAINS = ["www.elsevier.com"]
 
 
 def create_restricted_link_url_extractor(current_url):
     extractor = LinkExtractor(
         allow=ALLOWED_LINKS,
         deny=NOT_ALLOWED_LINKS,
-        allow_domains=get_allowed_domains(current_url),
+        allow_domains=create_allowed_domains_including_current_url(current_url),
         process_value=clean_url,
         unique=True,
     )
@@ -73,7 +69,7 @@ def create_restricted_link_text_extractor(current_url):
         restrict_text=ALLOWED_TEXT,
         process_value=clean_url,
         unique=True,
-        allow_domains=get_allowed_domains(current_url),
+        allow_domains=create_allowed_domains_including_current_url(current_url),
     )
     extractor.deny_extensions = [
         ext for ext in extractor.deny_extensions if ext not in ALLOWED_EXTENSIONS
@@ -81,5 +77,12 @@ def create_restricted_link_text_extractor(current_url):
     return extractor
 
 
-def get_allowed_domains(url):
-    return ALLOWED_DOMAINS.append(url)
+def create_allowed_domains_including_current_url(url):
+    domain = get_domain_from_url(url)
+    domains = [domain for domain in ALLOWED_DOMAINS]
+    domains.append(domain)
+    return domains
+
+
+def get_domain_from_url(url):
+    return urlparse(url).netloc

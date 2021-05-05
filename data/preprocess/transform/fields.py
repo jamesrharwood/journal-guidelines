5fdedba2e3954ec0ebee186e4f2c18cc2f0d2664
James Harwood <jamesrharwood@gmail.com>
2021-05-05 13:57:19 +0100

    Get root of starting URLs to profile distributions

--- data/preprocess/transform/fields.py
+++ data/preprocess/transform/fields.py
@@ -3,6 +3,7 @@ import re
 from data.fields import FIELDS
 from data.fields.abstract import AbstractField, AbstractListField
 from data.constants import PIVOT_FROM_COL
+from data.scrape.link_extractors.utils import get_root_from_url
 
 
 class TransformedFieldBase:
@@ -39,6 +40,18 @@ def is_english(languages):
     return "eng" in languages
 
 
+def count_publishers(publishers):
+    return len(publishers)
+
+
+def clean_publishers(publishers):
+    publishers = [publisher.strip(",") for publisher in publishers]
+
+
+def get_url_roots(urls):
+    return [get_root_from_url(url) for url in urls]
+
+
 FIELDS = [
     TransformedField(
         "is_periodical",
@@ -47,4 +60,6 @@ FIELDS = [
     ),
     TransformedField("is_english", FIELDS.languages_, is_english),
     TransformedListField(PIVOT_FROM_COL, FIELDS.urls_raw_, url_filter),
+    TransformedListField("publishers_cleaned", FIELDS.publisher_, clean_publishers),
+    TransformedListField("url_roots", PIVOT_FROM_COL, get_url_roots),
 ]
